8f90b8e1fb076b081e6349449ef1321ffebc38d0
brianpavon <53911453+brianpavon@users.noreply.github.com>
2020-04-20 00:52:04 -0300

    TP 1

--- TP_1_2020/Entidades/Numero.cs
+++ TP_1_2020/Entidades/Numero.cs
@@ -10,23 +10,36 @@ namespace Entidades
     {
         private double numero;
 
-
+        /// <summary>
+        /// Constructor sin parametro setea el atributo en 0
+        /// </summary>
         public Numero()
         {
             this.numero = 0;
         }
-
+        /// <summary>
+        /// Constructor que recibe un double y lo setea en el atributo numero
+        /// </summary>
+        /// <param name="numero"></param>
         public Numero(double numero)
         {
             this.numero = numero;
         }
         
+        /// <summary>
+        /// Constructor que recibe un string y lo setea en el atributo numero
+        /// </summary>
+        /// <param name="numero"></param>
         public Numero(string numero)
         {
             this.SetNumero = numero;
         }
 
-       
+       /// <summary>
+       /// Valida que el string recibido sea un numero
+       /// </summary>
+       /// <param name="strNumero"></param>
+       /// <returns>Devuelve el string validado en tipo double</returns>
         private double ValidarNumero(string strNumero)
         {
             double esNumero;
@@ -37,7 +50,9 @@ namespace Entidades
                
             return esNumero;
         }
-
+        /// <summary>
+        /// Setea el atributo numero luego de validarlo
+        /// </summary>
         public string SetNumero
         {
             set 
@@ -46,7 +61,12 @@ namespace Entidades
             }
            
         }
-
+        /// <summary>
+        /// Sobrecarga del operador + para realizar la suma entre 2 parametros de tipo double
+        /// </summary>
+        /// <param name="numUno">Primer elemento que se sumara</param>
+        /// <param name="numDos">Segundo elemento que se sumara</param>
+        /// <returns>Devuelve el resultado de la suma</returns>
         public static double operator +(Numero numUno,Numero numDos)
         {
             double resultado;
@@ -54,7 +74,12 @@ namespace Entidades
 
             return resultado;
         }
-
+        /// <summary>
+        /// Sobrecarga del operador - para realizar la resta entre 2 parametros de tipo double
+        /// </summary>
+        /// <param name="numUno">Primer elemento que se usara para la resta</param>
+        /// <param name="numDos">Segundo elemento que se usara para la resta</param>
+        /// <returns>Devuelve el resultado de la resta</returns>
         public static double operator -(Numero numUno, Numero numDos)
         {
             double resultado;
@@ -62,7 +87,12 @@ namespace Entidades
 
             return resultado;
         }
-
+        /// <summary>
+        /// Sobrecarga del operador * para realizar la multiplicacion entre 2 parametros de tipo double
+        /// </summary>
+        /// <param name="numUno">Primer elemento que se usara para la multiplicacion</param>
+        /// <param name="numDos">Segundo elemento que se usara para la multiplicacion</param>
+        /// <returns>Devuelve el resultado de la multiplicacion</returns>
         public static double operator *(Numero numUno, Numero numDos)
         {
             double resultado;
@@ -70,7 +100,12 @@ namespace Entidades
 
             return resultado;
         }
-
+        /// <summary>
+        /// Sobrecarga del operador / para realizar la division entre 2 parametros de tipo double
+        /// </summary>
+        /// <param name="numUno">Primer elemento que se usara para la division</param>
+        /// <param name="numDos">Segundo elemento que se usara para la division</param>
+        /// <returns>Devuelve el resultado de la division</returns>
         public static double operator /(Numero numUno, Numero numDos)
         {
             double resultado;
@@ -81,12 +116,15 @@ namespace Entidades
             else
             {
                 resultado = numUno.numero / numDos.numero;
-            }
-
-            
+            }            
             return resultado;
         }
 
+        /// <summary>
+        /// Transforma un binario a numero decimal
+        /// </summary>
+        /// <param name="numBinario">El string que contiene el numero binario a transformar</param>
+        /// <returns>Devuelve el numero decimal correspondiente al binario</returns>
         public static string BinarioDecimal(string numBinario)
         {
             string retorno;
@@ -108,7 +146,11 @@ namespace Entidades
 
             return retorno;           
         }    
-        
+        /// <summary>
+        /// Transforma un numero decimal a binario
+        /// </summary>
+        /// <param name="stringDecimal">El string que contiene el numero decimal a transfromar</param>
+        /// <returns>El numero binario correspondiente a ese decimal</returns>
         public static string DecimalBinario(string stringDecimal)
         {
             int numero = int.Parse(stringDecimal);
@@ -128,7 +170,11 @@ namespace Entidades
             }            
             return cadenaBinario;
         }
-
+        /// <summary>
+        /// Transforma un numero decimal a binario
+        /// </summary>
+        /// <param name="stringDecimal">El double que contiene el numero decimal a transfromar</param>
+        /// <returns>El numero binario correspondiente a ese decimal</returns>
         public static string DecimalBinario(double numeroDecimal)
         {
             string cadenaBinario = numeroDecimal.ToString();
